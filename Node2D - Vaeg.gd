09b72c8d78f22475efc652d8705fa894dde068cd
Kim Tiedemann <kim@tiede.dk>
2022-09-26 18:43:59 +0200

    Added collisions

--- Node2D - Vaeg.gd
+++ Node2D - Vaeg.gd
@@ -5,3 +5,13 @@ func _physics_process(delta):
 	if global_position.x <= -50:
 		queue_free()
 		print("vaeg fjernet")
+
+func _on_Area2D__Top_body_entered(body):
+	print("top ramt")
+	
+func _on_Area2D__Bund_body_entered(body):
+	print("bund ramt")
+
+
+func _on_Area2D_body_entered(body):
+	print("wuhu - point")
