85d551e172873e38c02e652e01ae30c72ef3309a
Kim Tiedemann <kim@tiede.dk>
2022-10-03 17:06:08 +0200

    Added random y position for walls

--- Node2D - Flere Vaegge.gd
+++ Node2D - Flere Vaegge.gd
@@ -4,6 +4,12 @@ onready var timer = $Timer
 
 var VAEG = preload("res://Node2D - Vaeg.tscn")
 
+func _ready():
+	randomize()
+
 func _on_Timer_timeout():
 	var vaeg = VAEG.instance()
 	add_child(vaeg)
+	var y = randi() % 170 + 1
+	print(y)
+	vaeg.position.y = y
