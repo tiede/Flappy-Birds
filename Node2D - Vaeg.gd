84d7c2446a7a64b01aa7692d72af62cdfba6a330
Kim Tiedemann <kim@tiede.dk>
2022-11-28 20:55:33 +0100

    Added file storage of high scores

--- Node2D - Vaeg.gd
+++ Node2D - Vaeg.gd
@@ -11,17 +11,19 @@ func _physics_process(delta):
 func _on_Area2D__Top_body_entered(body):
 	print("top ramt")
 	if !cheatMode:
-		game_over()
+		game_over(body)
 	
 func _on_Area2D__Bund_body_entered(body):
 	print("bund ramt")
 	if !cheatMode:
-		game_over()
+		game_over(body)
 
 func _on_Area2D_body_entered(body):
 	print("wuhu - point")
 	body.tilfoej_point()
 	$"PointsStreamPlayer".play()
 
-func game_over():
-	get_tree().change_scene("res://Node2D - Game over.tscn")
+func game_over(body):
+	print(body)
+	body.game_over()
+	#get_tree().change_scene("res://Node2D - Game over.tscn")
